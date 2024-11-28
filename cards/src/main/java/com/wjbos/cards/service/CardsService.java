c25e28500758ed3583da91b0017378f902d3f493
WJbos <wjbosdev@gmail.com>
2024-11-28 17:49:49 +0200

    Service and Impl for Business logic inside the Controller

--- cards/src/main/java/com/wjbos/cards/service/CardsService.java
+++ cards/src/main/java/com/wjbos/cards/service/CardsService.java
@@ -1,4 +1,15 @@
 package com.wjbos.cards.service;
 
+import com.wjbos.cards.dto.CardsDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+
 public interface CardsService {
+    CardsDto fetchByMobileNumber(String mobileNumber);
+
+    void createCardByMobileNumber(@Pattern(regexp = "(^$|[0-9]{10})",
+                                                          message = "Mobile Number needs to be 10 digits long") String mobileNumber);
+    boolean updateCardDetails(@Valid CardsDto cardsDto);
+
+    boolean deleteByMobileNumber(@Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number needs to be 10 digits") String mobileNumber);
 }
--- cards/src/main/java/com/wjbos/cards/service/impl/CardsServiceImpl.java
+++ cards/src/main/java/com/wjbos/cards/service/impl/CardsServiceImpl.java
@@ -1,6 +1,72 @@
 package com.wjbos.cards.service.impl;
 
+import com.wjbos.cards.constants.CardsConstants;
+import com.wjbos.cards.dto.CardsDto;
+import com.wjbos.cards.entity.Cards;
+import com.wjbos.cards.exceptions.CardAlreadyExistsException;
+import com.wjbos.cards.exceptions.ResourceNotFoundException;
+import com.wjbos.cards.mapper.CardsMapper;
+import com.wjbos.cards.repository.CardsRepository;
 import com.wjbos.cards.service.CardsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
 public class CardsServiceImpl implements CardsService {
+    private CardsRepository cardsRepository;
+
+    @Override
+    public CardsDto fetchByMobileNumber(String mobileNumber) {
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Card","MobileNumber",mobileNumber)
+        );
+       return CardsMapper.mapToCardDto(cards,new CardsDto());
+    }
+
+    @Override
+    public void createCardByMobileNumber(String mobileNumber) {
+       Optional<Cards> cards = cardsRepository.findByMobileNumber(mobileNumber);
+
+        if(cards.isPresent()){
+            throw new CardAlreadyExistsException("Card","MobileNumber",mobileNumber);
+        }
+            cardsRepository.save(createNewCard(mobileNumber));
+    }
+
+    private Cards createNewCard(String mobileNumber){
+        Cards cards = new Cards();
+        long randomAccountNumber = 100000000000L + new Random().nextInt(900000000);
+        cards.setCardNumber(Long.toString(randomAccountNumber));
+        cards.setMobileNumber(mobileNumber);
+        cards.setCardType(CardsConstants.CREDIT_CARD);
+        cards.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+        cards.setAmountUsed(0);
+        cards.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        return cards;
+    }
+
+    @Override
+    public boolean updateCardDetails(CardsDto cardsDto) {
+        Cards cards = cardsRepository.findByMobileNumber(cardsDto.getMobileNumber()).orElseThrow(
+                () -> new ResourceNotFoundException("Card","MobileNumber",cardsDto.getMobileNumber())
+        );
+       CardsMapper.mapToCardEntity(cardsDto,cards);
+       cardsRepository.save(cards);
+       return true;
+    }
+
+    @Override
+    public boolean deleteByMobileNumber(String mobileNumber) {
+       Optional<Cards>  cards = cardsRepository.findByMobileNumber(mobileNumber);
+        if(cards.isPresent()){
+            cardsRepository.delete(cards.get());
+            return true;
+        }else{
+            throw new ResourceNotFoundException("Card","MobileNumber",mobileNumber);
+        }
+    }
 }
