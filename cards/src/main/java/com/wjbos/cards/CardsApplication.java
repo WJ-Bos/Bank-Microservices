ba7113f13732c4be931e1f659687887e38231fa2
WJbos <wjbosdev@gmail.com>
2024-12-04 20:36:53 +0200

    Added the enableConfig annotation to cards

--- cards/src/main/java/com/wjbos/cards/CardsApplication.java
+++ cards/src/main/java/com/wjbos/cards/CardsApplication.java
@@ -1,5 +1,6 @@
 package com.wjbos.cards;
 
+import com.wjbos.cards.dto.CardsContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,10 +8,13 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CardsContactInfoDto.class})
 @OpenAPIDefinition(
         info = @Info(
                 title = "Cards Microservice Documentation",
