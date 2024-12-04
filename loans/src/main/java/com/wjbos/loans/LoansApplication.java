ce24fda1f3c09003509607f119d88750431719c0
WJbos <wjbosdev@gmail.com>
2024-12-04 20:36:46 +0200

    Added the enableConfig annotation to loans

--- loans/src/main/java/com/wjbos/loans/LoansApplication.java
+++ loans/src/main/java/com/wjbos/loans/LoansApplication.java
@@ -1,10 +1,13 @@
 package com.wjbos.loans;
 
+import com.wjbos.loans.dto.LoansContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class LoansApplication {
 
